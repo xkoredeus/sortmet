289e36f648c6aad4193b10f72354f02dbcf7e833
xkored <xkored@gmail.com>
2019-04-30 09:38:28 +0400

    third commit

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -5,16 +5,6 @@ $(function() {
       loop: false,
       dots: true,
       navText: ["<img src='img/slider__arrow_prev.png'>", "<img src='img/slider__arrow_next.png'>"],
-  });
-	$('.team__cnt').owlCarousel({
-      nav: false,
-      dots: true,
-      items: 5,
-      loop: true,
-      margin: 20,
-      center:true,
-      autoWidth:true,
-      mouseDrag: false,
       responsive : {
         0   : {
             items: 1
@@ -33,33 +23,21 @@ $(function() {
         }
       }
 	});
-	// $(".tab-content__item").hide();
-	$(".tabs__container div.tab-content__item:first-child").addClass('active');
-	$(".main-slider .main-slider__bg-item:first-child").addClass('active');
-	    $('ul.tabs__list > li').click(function () {
-	        if (!($(this).hasClass('active'))) {
-	            var thisLi = $(this);
-	            var numLi = thisLi.index();
-	            thisLi.addClass('active').siblings().removeClass('active');
-	            thisLi.parent().next().children('div').removeClass('active').eq(numLi).addClass('active','slow');
-	            thisLi.parent().parent().parent().parent().find('.main-slider__bg').children('.main-slider__bg-item').removeClass('active','slow').eq(numLi).addClass('active','slow');
-	            $('.reviews__cnt_text').owlCarousel('refresh');
-	        }
+	// tabs 
+  $(document).ready(function () {
+    $(".tabs-content__item:not(:first-child)").hide();
+    $(".tabs__container div.tabs__content-item.active-tab").show();
+    $('ul.tabs__list > li').click(function () {
+      if (!($(this).hasClass('active'))) {
+        var thisLi = $(this);
+        var numLi = thisLi.index();
+        thisLi.addClass('active').siblings().removeClass('active');
+        thisLi.parent().next().children('div').hide().eq(numLi).fadeIn('slow');
+      }
     });
-  //бегушая строка
-  $(function() {
-    var marquee = $(".running-string__item"); 
-    marquee.css({"overflow": "hidden", "width": "100%"});
-    // оболочка для текста ввиде span (IE не любит дивы с inline-block)
-    marquee.wrapInner("<span>");
-    marquee.find("span").css({ "width": "50%", "display": "inline-block", "text-align":"center" }); 
-    marquee.append(marquee.find("span").clone()); // тут у нас два span с текстом
-    marquee.wrapInner("<div>");
-    marquee.find("div").css("width", "200%");
-    var reset = function() {
-      $(this).css("margin-left", "0%");
-      $(this).animate({ "margin-left": "-100%" }, 12000, 'linear', reset);
-    };
-    reset.call(marquee.find("div"));
   });
+  //Закрываем AjaxForm popup после успешной отправки
+  // $(document).on('af_complete', function(event,res) {
+  //   if(res.success) parent.$.fancybox.close();
+  // });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -13,8 +13,8 @@ var gulp       = require('gulp'), // Подключаем Gulp
 
 gulp.task('sass', function(){ // Создаем таск Sass
 	return gulp.src('app/sass/**/*.sass') // Берем источник
-		.pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
-		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
+		.pipe(sass({outputStyle: 'expanded'})) // Преобразуем Sass в CSS посредством gulp-sass
+		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: false })) // Создаем префиксы
 		.pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
 		.pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
 });
