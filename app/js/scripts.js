81083efbf7b313c2152445b89d57d3cd8ba20c31
xkored@gmail.com <ats28325>
2019-12-10 15:02:46 +0400

    feat: card

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -53,3 +53,67 @@ $(() => {
     },
   });
 });
+$(() => {
+  $('.more__in').owlCarousel({
+    nav: true,
+    loop: false,
+    smartSpeed: 800,
+    startPosition: 0,
+    dots: false,
+    margin: 15,
+    navText: ["<svg width='7' height='11' viewBox='0 0 11 18' xmlns='http://www.w3.org/2000/svg'><path d='M10 1L2 9L10 17' stroke-width='2'/></svg>","<svg width='7' height='11' viewBox='0 0 11 18' xmlns='http://www.w3.org/2000/svg'><path d='M1 17L9 9L1 0.999999' stroke-width='2'/></svg>"],
+    responsive : {
+      0   : {
+          items: 1,
+      },
+      600 : {
+          items: 2,
+      },
+      900 : {
+          items: 3,
+      },
+      1200 : {
+          items: 5,
+      }
+    },
+  });
+});
+$(() => {
+  $('.watch__in').owlCarousel({
+    nav: true,
+    loop: false,
+    smartSpeed: 800,
+    startPosition: 0,
+    dots: false,
+    margin: 15,
+    navText: ["<svg width='7' height='11' viewBox='0 0 11 18' xmlns='http://www.w3.org/2000/svg'><path d='M10 1L2 9L10 17' stroke-width='2'/></svg>","<svg width='7' height='11' viewBox='0 0 11 18' xmlns='http://www.w3.org/2000/svg'><path d='M1 17L9 9L1 0.999999' stroke-width='2'/></svg>"],
+    responsive : {
+      0   : {
+          items: 1,
+      },
+      600 : {
+          items: 2,
+      },
+      900 : {
+          items: 3,
+      },
+      1200 : {
+          items: 5,
+      }
+    },
+  });
+});
+// tabs
+$(() => {
+  $('.tabs__content-item:not(:first-child)').hide();
+  $('.tabs__list > div').click(function tabListLiClick() {
+    if (!($(this).hasClass('active'))) {
+      const thisLi = $(this);
+      const numLi = thisLi.index();
+      thisLi.addClass('active').siblings().removeClass('active');
+      thisLi.parent().next().children('div').hide()
+        .eq(numLi)
+        .fadeIn('slow');
+    }
+  });
+});
