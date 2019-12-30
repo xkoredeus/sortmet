e7147826f8c64d81221302639fda0e0582781bbd
xkored <xkored@gmail.com>
2019-12-30 15:36:08 +0400

    feat: contacts vacancy

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -197,3 +197,11 @@ $(() => {
     catFilterCheckbox.show();
   })
 });
+// input type file
+$('.upload__file').change(function() {
+  var filename = [];
+  for (var i = 0; i < $(this).get(0).files.length; ++i) {
+    filename.push($(this).get(0).files[i].name.slice(0, 18) + '...');
+  }
+  $(this).next('.filename').text(filename.join(', '));
+});
